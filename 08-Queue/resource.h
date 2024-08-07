dcf79ee9c99fbbe0977f13c1331fbc7a5281f9b4
Hayden Zhou <mfkhao2009@outlook.com>
2024-08-08 07:59:08 +0800

    08-Queue

---
+++ 08-Queue/resource.h
@@ -0,0 +1,20 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by 08-Queue.rc
+//
+#define IDD_QUEUE                       101
+#define IDC_SERVERS                     1001
+#define IDC_CLIENTS                     1003
+#define IDC_BUTTON1                     1004
+#define IDC_BTN_STOP                    1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
